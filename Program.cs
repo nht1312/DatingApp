1dacd9528a8b600c16d209f0065208504dcf77cb
itnhtu <itnhtu@gmail.com>
2023-06-26 13:41:28 +0700

    connect sql, write api using entity framework

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-
+//Connect SQLite
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
