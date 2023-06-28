5803d5a756308d1649fe0304a337a7ec2211b0ec
itnhtu <itnhtu@gmail.com>
2023-06-28 09:31:58 +0700

    angular routing, add bootstrap theme, using angular route guards, using shared module

---
+++ src/app/_modules/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center',
+      progressBar: true,
+    }),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService,private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -22,7 +23,11 @@ export class RegisterComponent implements OnInit {
       next: () => {
         this.cancel();
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error);
+        console.log(error);
+        
+      }
     })
   }
   //cancel
