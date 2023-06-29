cb9b1d201e62e0be4a79c1b0faa7187d9b201963
itnhtu <itnhtu@gmail.com>
2023-06-29 13:56:42 +0700

    using ts, interceptor to send jwt tokens, using 3party photo gallery

---
+++ src/app/_models/member.ts
@@ -0,0 +1,24 @@
+import { Photo } from './photo';
+
+export interface Member {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    age: number;
+    knownAs: string;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: Photo[];
+  }
+
+//   export interface Photo {
+//     id: number;
+//     url: string;
+//     isMain: boolean;
+// }
