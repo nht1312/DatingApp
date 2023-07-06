f7d181086bffa4d646e0565cbdebce9f1d1e6303
itnhtu <itnhtu@gmail.com>
2023-07-06 16:18:17 +0700

    implement likes repo, add method into LikesController, adding pagination

--- Data/LikeRepository.cs
+++ Data/LikeRepository.cs
@@ -1,25 +1,60 @@
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class LikeRepository : ILikeRepository
     {
-        
-        public Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        private readonly DataContext _context;
+        public LikeRepository(DataContext Context)
         {
-            throw new NotImplementedException();
+            _context = Context;
+            
+        }
+        public async Task<UserLike> GetUserLike(int sourceUserId, int targetUserId)
+        {
+            return await _context.Likes.FindAsync(sourceUserId, targetUserId);
         }
 
-        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
         {
-            throw new NotImplementedException();
+            //truy van
+            var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
+            var likes = _context.Likes.AsQueryable();
+
+            if(likesParams.Predicate == "liked")
+            {
+                likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
+                users = likes.Select(like => like.TargetUser);
+            }
+            if(likesParams.Predicate == "likedBy")
+            {
+                likes = likes.Where(like => like.TargetUserId == likesParams.UserId);
+                users = likes.Select(like => like.SourceUser);
+            }
+
+            var likedUsers = users.Select(user => new LikeDto {
+                UserName = user.UserName,
+                KnowAs = user.KnowAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            });
+            
+            return await PagedList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
+
         }
 
-        public Task<AppUser> GetUserWithLikes(int userId)
+        public async Task<AppUser> GetUserWithLikes(int userId)
         {
-            throw new NotImplementedException();
+            return await _context.Users
+                .Include(x => x.LikedUsers)
+                .FirstOrDefaultAsync(x => x.Id == userId);
         }
     }
 }
